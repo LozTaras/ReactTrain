d575216b3d9f4ac71fcc87e674156d7a836def5e
Taras Lozinskiy <t.lozinskiy@astoundcommerce.com>
2019-02-07 10:55:35 +0200

    07.02.2019

--- src/components/Article.js
+++ src/components/Article.js
@@ -2,41 +2,20 @@ import React, {PureComponent} from 'react';
 
 class Article extends PureComponent {
     state = {
-        isOpen: this.props.defaultOpen,
         count: 0
     }
 
-    /*shouldComponentUpdate(nextProps, nextState) {
-        return this.state.isOpen !== nextState.isOpen;
-    }*/
-
-    componentWillMount() {
-        console.log('---', 'mounting');
-    }
-
-    componentWillReceiveProps(newProps) {
-        if(newProps.defaultOpen !== this.props.defaultOpen) {
-            this.setState({
-                isOpen: newProps.defaultOpen
-            });
-        }
-    }
-
-    componentWillUpdate() {
-        console.log('---', 'willUpdate');
-    }
-
     render() {
-        const {article} = this.props,
-            body = this.state.isOpen && <section>{article.text}</section>;
+        const {article, isOpen, onButtonClick} = this.props,
+            body = isOpen && <section>{article.text}</section>;
 
         return (
             <div>
-                <h2 onClick = {this.incrementCounter}>                    
+                <h2 onClick = {this.incrementCounter} onMouseDown = {this.preventDefault}>                    
                     {article.title}          
                     clicked {this.state.count}          
-                    <button onClick={this.handleClick}>
-                        {this.state.isOpen ? 'close' : 'open'}
+                    <button onClick={onButtonClick}>
+                        {isOpen ? 'close' : 'open'}
                     </button>  
                 </h2>
                 {body}
@@ -45,17 +24,14 @@ class Article extends PureComponent {
         )
     }
 
-    handleClick = () => {
-        console.log('---', 'clicked');
-        this.setState({
-            isOpen: !this.state.isOpen
-        });
-    }
-
     incrementCounter = () => {
         this.setState({
             count: this.state.count + 1
         });
+    }
+
+    preventDefault = evt => {
+        evt.preventDefault();
     }
 }
 
--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,9 +2,20 @@ import React, {PureComponent} from 'react';
 import Article from './Article';
 
 class ArticleList extends PureComponent {
+    state = {
+        openArticleId: null
+    }
+
     render() {
         const articlesElments = this.props.articles.map((article, index) => {
-            return <li key = {article.id}><Article article={article} defaultOpen={index === 0}/></li>
+            return (
+                <li key = {article.id}>
+                    <Article article={article} 
+                        isOpen = {this.state.openArticleId === article.id}
+                        onButtonClick ={this.handleClick.bind(this, article.id)}
+                        />
+                </li>
+            )
         });
     
         return (
@@ -12,7 +23,11 @@ class ArticleList extends PureComponent {
                 {articlesElments}
             </ul>
         )
-    }    
+    }
+    
+    handleClick = openArticleId => this.setState({ 
+        openArticleId: this.state.openArticleId === openArticleId ? null : openArticleId
+    });
 }
 
 export default ArticleList;
